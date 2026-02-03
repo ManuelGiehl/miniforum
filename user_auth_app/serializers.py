93525fd6d31cf76c9ea36cfcd667db8787c9824c
Manuel <manuzcore1@gmail.com>
2026-02-03 13:34:41 +0100

    feat: added serializers for user registration

---
+++ user_auth_app/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "repeated_password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already taken.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already registered.")
+        return value
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["repeated_password"]:
+            raise serializers.ValidationError({"repeated_password": "Passwords don't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("repeated_password")
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
