24f38b8c943f5c9f3ebf83357cdecb56f16afe2b
Manuel <manuzcore1@gmail.com>
2026-02-03 15:28:19 +0100

    feat: register post and comment in admin migrations applied

--- forum_app/admin.py
+++ forum_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_at"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["post", "author", "created_at"]
