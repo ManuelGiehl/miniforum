35b29b613f6cf92a968f36413e26ef9a7f466651
Manuel <manuzcore1@gmail.com>
2026-02-03 14:44:49 +0100

    feat: added set author in perform_create and add IsOwnerOrReadOnly to detail views

--- forum_app/views.py
+++ forum_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 
 from .models import Comment, Post
+from .permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer, PostSerializer
 
 
@@ -8,17 +9,25 @@ class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
