b678ebb005f5fe41fc6b06b4ef365d54f9fc95e5
Manuel <manuzcore1@gmail.com>
2026-02-03 13:46:14 +0100

    feat: add auth login via obtain_auth_token

--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .serializers import UserRegistrationSerializer
 
@@ -7,3 +9,9 @@ from .serializers import UserRegistrationSerializer
 class RegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny]
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def login_view(request):
+    return obtain_auth_token(request)
