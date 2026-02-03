ec54422a4ab315565c33a16a89d8899802e69dfc
Manuel <manuzcore1@gmail.com>
2026-02-03 13:36:34 +0100

    feat: added views for registration with generics

--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer
+
+
+class RegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
